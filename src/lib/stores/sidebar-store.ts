5f834c6865200324a3739d8f7ba566098cc86470
Junaid7200 <junaidjaffery1@gmail.com>
2026-03-25 11:24:00 +0500

    feat(state): add sidebar collapse store

---
+++ src/lib/stores/sidebar-store.ts
@@ -0,0 +1,17 @@
+import { create } from "zustand"
+
+type SidebarStore = {
+    collapsed: boolean
+    setCollapsed: (collapsed: boolean) => void
+    toggleCollapsed: () => void
+}
+
+export const useSidebarStore = create<SidebarStore>((set) => ({
+    collapsed: false,
+    setCollapsed: (collapsed) => {
+        set({collapsed})
+    },
+    toggleCollapsed: () => {
+        set((state) => ({collapsed: !state.collapsed}))
+    },
+}))
