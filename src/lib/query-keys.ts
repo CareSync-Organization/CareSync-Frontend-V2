f9ab85507e46b127e2b2982e1baf803b6ec370e5
Junaid7200 <junaidjaffery1@gmail.com>
2026-06-11 15:49:05 +0500

    feat(knowledge-base): setup query keys for knowledge base

--- src/lib/query-keys.ts
+++ src/lib/query-keys.ts
@@ -1,8 +1,8 @@
 export const queryKeys = {
     knowledgeBase: {
         all: ["knowledge-base"] as const, // invalidate when logging out
-        list: (storeId: string) => [...queryKeys.knowledgeBase, "list", {storeId} ] as const,
-        detail: (documentId: string) => [...queryKeys.knowledgeBase, "detail", documentId] as const
+        list: (storeId: string) => [...queryKeys.knowledgeBase.all, "list", {storeId} ] as const,
+        detail: (documentId: string) => [...queryKeys.knowledgeBase.all, "detail", documentId] as const
     }
     // auth: ["auth"] as const,
     // currentUser: ["auth", "current-user"] as const,
